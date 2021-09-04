2d3d752392d4f52ffa3a04d01a4aebea72034e72
Michael <mrwolf.lmriii@gmail.com>
2021-09-04 15:52:04 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const btn = document.querySelector('.clear-task');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+loadEventListeners();
+
+function loadEventListeners(){
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(event){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+
+    event.preventDefault();
+}
